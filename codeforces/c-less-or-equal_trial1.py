f8dd94d2edaaae383d7965c997b5abefc4d555f1
Takieddin <43403458+Takieddin@users.noreply.github.com>
2026-03-23 20:00:21 +0100

    Add solution for C - Less or Equal (trial 1)

---
+++ codeforces/c-less-or-equal_trial1.py
@@ -0,0 +1,16 @@
+n,k = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+if k == 0:
+    if a[0] == 1:
+        print(-1)
+    else:
+        print(1)
+else:
+    if k == n:
+        print(a[-1])
+    else:
+        if a[k-1] == a[k]:
+            print(-1)
+        else:
+            print(a[k-1])
